50cae406478dc56c2aee18fc947a6897c6a63482
Baekshook <ecgo0106@naver.com>
2023-04-04 15:26:44 +0900

    Main component 추출 및 Router 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import ContactMe from "./components/ContactMe";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Introduce from "./components/Introduce";
-import Portfolio from "./components/Portfolio";
+import Main from "./pages/main";
 
 function App() {
   return (
-    <div className="bg-red-100 min-h-screen">
-      <Header />
-      <main>
-        <Introduce />
-        <Portfolio />
-        <ContactMe />
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="bg-red-100 min-h-screen">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Introduce from '../components/Introduce';
+import Portfolio from '../components/Portfolio';
+import ContactMe from '../components/ContactMe';
+
+export default function Main() {
+  return (
+    <main>
+      <Introduce />
+      <Portfolio />
+      <ContactMe />
+    </main>
+  );
+}
